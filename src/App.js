4667ee67030852712f7b08dc19dc49541ed239b9
farah mostafa <farahmostafa459@gmail.com>
2021-12-12 04:02:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import CinemaRoom from "./component/cinemaRoom/CinemaRoom"
+import React from 'react';
+import {BrowserRouter as Router, Routes , Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+        <Route exact path='/' element={< CinemaRoom />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
