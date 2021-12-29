198e8e0cc78be318bab584232a9277a3974dc712
farah mostafa <farahmostafa459@gmail.com>
2021-12-29 04:26:58 +0200

    changing services file location

--- src/services/screenRoom.js
+++ src/services/screenRoom.js
@@ -31,13 +31,13 @@ export async function AddReservations(id,reservedChairsId){
         return(response)
     } catch (error){
         if (error.response){
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
         } else if (error.request){
-          console.log(error.request);
+        console.log(error.request);
         } else {
-          console.log('Error', error.message);
+        console.log('Error', error.message);
         }
         console.log(error);
     }
