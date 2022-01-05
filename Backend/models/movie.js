a5f91ed8da05fde5c441daa61c0bf2ae0dcff4ac
Ayahossam95 <ayahossam_95@hotmail.com>
2022-01-05 18:49:09 +0200

    added 4 (Aya) APIs from the google documents

---
+++ Backend/models/movie.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true, // title is the key
+        unique: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    poster: {
+        type: String,
+        required: true
+    },
+    screeningRoom: {
+        type: String,
+        required: true
+    },
+    startTime: {
+        type: Date,
+        require: true,
+    },
+    endTime: {
+        type: Date,
+        require: true,
+    }
+});
+  
+module.exports = mongoose.model('Movie', movieSchema);
