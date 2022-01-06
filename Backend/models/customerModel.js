b3341af3ac3cb8316c2b43b920c01c48909f3c89
basantloay <basantloay2012@gmail.com>
2022-01-06 10:42:20 +0200

    Adding Encrypted passwords to Customers

--- Backend/models/customerModel.js
+++ Backend/models/customerModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
+const SALT = 12;
 const customerSchema = new mongoose.Schema({
   
   firstName: {
@@ -49,6 +50,40 @@ const customerSchema = new mongoose.Schema({
     select: false
   }
 });
+
+customerSchema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with salt cost of 12
+  this.password = await bcrypt.hash(this.password, SALT);
+  next();
+});
+
+customerSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+customerSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+customerSchema.methods.correctPassword = async function (pass,tokenpass)
+ {
+  return await bcrypt.compareSync(pass, tokenpass);
+};
 
 const Customer = mongoose.model('Customer', customerSchema);
 
--- Backend/testing/testCustomer.js
+++ Backend/testing/testCustomer.js
@@ -6,12 +6,40 @@ con.startConnection();
 const customer1=new Customer({
     firstName:'Basant',
     lastName:'Loay',
-    username:'basantloay9',
+    username:'basant1',
     email:'basantloay2012@gmail.a',
     password:'Abcd123456'
 });
 console.log('Laaaaaaaaa');
 customer1.save().then(doc=>{
+    console.log(doc);
+}).catch(err=>{
+console.log("ERRRROOORRRR : ",err);
+});
+
+const customer2=new Customer({
+    firstName:'Basant',
+    lastName:'Loay',
+    username:'basantloay9',
+    email:'basa2012@gmail.com',
+    password:'Abcd123456'
+});
+console.log('Laaaaaaaaa');
+customer2.save().then(doc=>{
+    console.log(doc);
+}).catch(err=>{
+console.log("ERRRROOORRRR : ",err);
+});
+
+const customer3=new Customer({
+    firstName:'Basant',
+    lastName:'Loay',
+    username:'basssant99',
+    email:'basant@gmail.com',
+    password:'Abcd123456'
+});
+console.log('Laaaaaaaaa');
+customer3.save().then(doc=>{
     console.log(doc);
 }).catch(err=>{
 console.log("ERRRROOORRRR : ",err);
